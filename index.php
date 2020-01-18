02ef30bbbb60a99c8fdfd2d3d07cff3d3b4e08b0
HewawasamM <56576219+HewawasamM@users.noreply.github.com>
2020-01-18 12:15:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index Page</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="wrapper">
+			<div class="logo">
+				<img src="image1.png" alt="">
+			</div>
+			<ul class="nav-area">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Service</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">ContactUs</a></li>
+			</ul>
+		</div>
+		<div class ="welcome-text">
+			<h1>Material Management</h1>
+			<a href="#">User Login</a>
+			<a href="#">Admin Login</a>
+		</div>
+	</header> 
+</body>
+</html>
